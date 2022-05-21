96e0134ec27281b28ed9cbc8ad4168c1a905d14b
Dipti-S1 <67460209+Dipti-S1@users.noreply.github.com>
2022-05-21 14:45:02 +0530

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,18 @@
+##Plot 1
+
+## Assuming that summary file is in data folder
+
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+library(ggplot2)
+total_emissions_per_year <- aggregate(NEI$Emissions, list(NEI$year), sum)
+colnames(total_emissions_per_year) <- c("Year", "Total")
+total_emissions_per_year
+
+##   Year   Total
+## 1 1999 7332967
+## 2 2002 5635780
+## 3 2005 5454703
+## 4 2008 3464206
+
+ggplot(total_emissions_per_year, aes(x=Year,y=Total)) + labs(x="Year", y="Total PM2.5 emission from all sources", title="Total PM2.5 emission from all sources for each year") + geom_bar(width=0.8, stat="identity", fill="#5b23ad")
